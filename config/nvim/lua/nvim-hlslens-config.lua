c9e353cd65063b8ab9e888d3808bff5d5722a8b6
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2022-10-09 03:15:26 +0200

    Add nvim-hlslens and nvim-scrollbar

---
+++ config/nvim/lua/nvim-hlslens-config.lua
@@ -0,0 +1,22 @@
+local funcs = {}
+
+funcs.setup_keybindings = function()
+  local kmap = vim.api.nvim_set_keymap
+  local opts = { noremap = true, silent = true }
+
+  kmap('n', 'n',
+      [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]],
+      opts)
+  kmap('n', 'N',
+      [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]],
+      opts)
+  kmap('n', '*', [[*<Cmd>lua require('hlslens').start()<CR>]], opts)
+  kmap('n', '#', [[#<Cmd>lua require('hlslens').start()<CR>]], opts)
+  kmap('n', 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>]], opts)
+  kmap('n', 'g#', [[g#<Cmd>lua require('hlslens').start()<CR>]], opts)
+
+  kmap('n', '<Leader>l', ':noh<CR>', opts)
+end
+
+
+return funcs
--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -17,95 +17,130 @@ packer.startup(function(use)
   use 'nvim-lua/plenary.nvim'
 
   -- Theme & Icons
-  use { 'folke/tokyonight.nvim',
-        branch = 'main',
-        config = function() require('tokyonight-config').setup() end
-      }
+  local tokyonight = { 'folke/tokyonight.nvim',
+    branch = 'main',
+    config = function() require('tokyonight-config').setup() end
+  }
+  use(tokyonight)
 
   -- File Tree
   use { 'kyazdani42/nvim-tree.lua',
-        config = function()
-          local config = require('nvim-tree-config')
-          config.setup()
-          config.setup_keybindings()
-        end,
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-      }
+    config = function()
+      local config = require('nvim-tree-config')
+      config.setup()
+      config.setup_keybindings()
+    end,
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
 
   -- Status Line
   use { 'nvim-lualine/lualine.nvim',
-        config = function() require('nvim-lualine-config').setup() end,
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-      }
+    config = function() require('nvim-lualine-config').setup() end,
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
 
   -- KNAP - Auto Refresh Preview for LaTeX, Markdown, HTML etc.
   use { 'frabjous/knap',
-        config = function()
-          local config = require('knap-config')
-          config.setup()
-          config.setup_keybindings()
-        end
-      }
+    config = function()
+      local config = require('knap-config')
+      config.setup()
+      config.setup_keybindings()
+    end
+  }
 
   -- LSP - Language Server Protocol
   use { 'neovim/nvim-lspconfig',
-        config = function () require('nvim-lspconfig-config').setup() end,
-        requires = { 'hrsh7th/cmp-nvim-lsp' }
-      }
+    config = function () require('nvim-lspconfig-config').setup() end,
+    requires = { 'hrsh7th/cmp-nvim-lsp' }
+  }
 
   -- Code Snippets
   use 'rafamadriz/friendly-snippets'
   use { 'L3MON4D3/LuaSnip', tag = "v1.*",
-        config = function ()
-          require('luasnip.loaders.from_vscode').lazy_load()
-        end
-      }
+    config = function ()
+      require('luasnip.loaders.from_vscode').lazy_load()
+    end
+  }
 
   -- Code Completion
   use { 'hrsh7th/nvim-cmp',
-        config = function() require('nvim-cmp-config').setup() end,
-        requires = {
-          { 'hrsh7th/cmp-path' },
-          { 'hrsh7th/cmp-buffer' },
-          { 'hrsh7th/cmp-cmdline' },
-          { 'hrsh7th/cmp-nvim-lsp-signature-help' },
-          { 'hrsh7th/cmp-nvim-lsp-document-symbol' },
-          { 'L3MON4D3/LuaSnip', tag = "v<CurrentMajor>.*" },
-          { 'saadparwaiz1/cmp_luasnip' },
-          { 'lukas-reineke/cmp-rg' }
-        }
-      }
+    config = function() require('nvim-cmp-config').setup() end,
+    requires = {
+      { 'hrsh7th/cmp-path' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-cmdline' },
+      { 'hrsh7th/cmp-nvim-lsp-signature-help' },
+      { 'hrsh7th/cmp-nvim-lsp-document-symbol' },
+      { 'L3MON4D3/LuaSnip', tag = "v<CurrentMajor>.*" },
+      { 'saadparwaiz1/cmp_luasnip' },
+      { 'lukas-reineke/cmp-rg' }
+    }
+  }
 
   use { 'windwp/nvim-autopairs',
-        config = function() require('nvim-autopairs-config').setup() end,
-        requires = { 'hrsh7th/nvim-cmp' }
-      }
+    config = function() require('nvim-autopairs-config').setup() end,
+    requires = { 'hrsh7th/nvim-cmp' }
+  }
 
   -- Treesitter
   use { 'nvim-treesitter/nvim-treesitter',
-        config = function () require('nvim-treesitter-config').setup() end,
-        run = ':TSUpdate',
-        requires = {
-          { 'nvim-treesitter/nvim-treesitter-context',
-            config = function () require('nvim-treesitter-context-config').setup() end
-          },
-          { 'nvim-treesitter/nvim-treesitter-refactor' }
-        }
-      }
+    config = function () require('nvim-treesitter-config').setup() end,
+    run = ':TSUpdate',
+    requires = {
+      { 'nvim-treesitter/nvim-treesitter-context',
+        config = function () require('nvim-treesitter-context-config').setup() end
+      },
+      { 'nvim-treesitter/nvim-treesitter-refactor' }
+    }
+  }
 
   -- Fuzzy Finder
   use { 'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        config = function () require('telescope-config').setup_keybindings() end,
-        requires = {
-          { 'nvim-lua/plenary.nvim' },
-          { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
-        }
-      }
+    config = function () require('telescope-config').setup_keybindings() end,
+    requires = {
+      { 'nvim-lua/plenary.nvim' },
+      { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+    }
+  }
 
   -- Effects
-  use {
-    "folke/twilight.nvim",
+  use { 'folke/twilight.nvim',
     config = function() require("twilight-config").setup() end
+  }
+
+  -- Search
+  local hlslens = { 'kevinhwang91/nvim-hlslens',
+    config = function() require('nvim-hlslens-config').setup_keybindings() end
+  }
+  use(hlslens)
+
+  -- Scrolling
+  use { 'karb94/neoscroll.nvim',
+    config = function() require('neoscroll').setup({ cursor_scrolls_alone = true }) end
+  }
+
+  -- Scrollbar
+  use { 'petertriho/nvim-scrollbar',
+    config = function()
+      -- Set up tokyonight colors for scrollbar
+      local colors = require('tokyonight.colors').setup()
+      require('scrollbar').setup({
+        handle = {
+            color = colors.bg_highlight,
+        },
+        marks = {
+            Search = { color = colors.orange },
+            Error = { color = colors.error },
+            Warn = { color = colors.warning },
+            Info = { color = colors.info },
+            Hint = { color = colors.hint },
+            Misc = { color = colors.purple },
+        }
+      })
+      -- Set up hlslens integration
+      require('scrollbar.handlers.search').setup({ calm_down = true, nearest_only = true })
+    end,
+    requires = { hlslens, tokyonight }
   }
 
 end)
