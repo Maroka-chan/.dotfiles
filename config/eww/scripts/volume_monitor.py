45829b53d87b4cb7dbe3e63b57c7ae17865c0dc9
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2023-07-05 01:51:35 +0200

    Add eww config

--- config/eww/scripts/volume_monitor.py
+++ config/eww/scripts/volume_monitor.py
@@ -1,5 +1,7 @@
 import os
+import sys
 
-output = os.popen('pw-cli | grep --line-buffered "remote 0 node 58 changed"')
+output = os.popen('pw-cli | grep --line-buffered "remote 0 node 57 changed"')
 while(output.readline()):
     os.system("wpctl get-volume @DEFAULT_AUDIO_SINK@ | cut -d' ' -f2")
+    sys.stdout.flush()
--- config/eww/scripts/volume_monitor.sh
+++ config/eww/scripts/volume_monitor.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Set value initially
+wpctl get-volume @DEFAULT_AUDIO_SINK@ | cut -d' ' -f2
+
+# Monitor pipewire for changes to the default sink and print the new volume when a change occures
 DEFAULT_SINK=$(wpctl status | rg '\*' -m1 | cut -d'*' -f2 | xargs | cut -d'.' -f1)
 
 pw-mon | rg --line-buffered 'changed:' -A1 | rg --line-buffered "id: ${DEFAULT_SINK}" | 
