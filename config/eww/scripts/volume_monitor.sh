8babe35810164a25aaf2721e6cfcf26b86137625
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2023-07-11 02:12:02 +0200

    Add option to check if volume is muted

--- config/eww/scripts/volume_monitor.sh
+++ config/eww/scripts/volume_monitor.sh
@@ -1,12 +1,34 @@
-#!/bin/sh
-
-# Set value initially
-wpctl get-volume @DEFAULT_AUDIO_SINK@ | cut -d' ' -f2
+#!/usr/bin/env bash
 
 # Monitor pipewire for changes to the default sink and print the new volume when a change occures
 DEFAULT_SINK=$(wpctl status | rg '\*' -m1 | cut -d'*' -f2 | xargs | cut -d'.' -f1)
 
-pw-mon | rg --line-buffered 'changed:' -A1 | rg --line-buffered "id: ${DEFAULT_SINK}" | 
-	while read; do 
-		wpctl get-volume @DEFAULT_AUDIO_SINK@ | cut -d' ' -f2
-	done
+if [ "$1" = "get-volume" ]; then
+	# Set value initially
+	wpctl get-volume @DEFAULT_AUDIO_SINK@ | cut -d' ' -f2
+
+	pw-mon | rg --line-buffered 'changed:' -A1 | rg --line-buffered "id: ${DEFAULT_SINK}" | 
+		while read; do 
+			wpctl get-volume @DEFAULT_AUDIO_SINK@ | cut -d' ' -f2
+		done
+fi
+
+if [ "$1" = "get-muted" ]; then
+	# Set value initially
+	muted=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | cut -d' ' -f3)
+	if [ "$muted" = "[MUTED]" ]; then
+		echo true
+	else
+		echo false
+	fi
+
+	pw-mon | rg --line-buffered 'changed:' -A1 | rg --line-buffered "id: ${DEFAULT_SINK}" | 
+		while read; do 
+			muted=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | cut -d' ' -f3)
+			if [ "$muted" = "[MUTED]" ]; then
+				echo true
+				continue
+			fi
+			echo false
+		done
+fi
